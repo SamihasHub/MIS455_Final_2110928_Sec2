1f308d85d324fad6eff1506e736a9e13710315f1
Samiha <2110928@iub.edu.bd>
2025-05-08 17:59:32 +0600

    Final JS

--- script.js
+++ script.js
@@ -21,6 +21,20 @@ function process(data) {
                 const currencyKey = Object.keys(country.currencies)[0];
                 currency = `${country.currencies[currencyKey].name} (${country.currencies[currencyKey].symbol || ""})`;
             }
+            
+        var newDiv = document.createElement("div");
+        newDiv.classList.add("innerStyle");
 
-        }
+        newDiv.innerHTML = `
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}" alt="Flag" width="150"><br><br>
+            <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
+            <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+            <p><strong>Currency:</strong> ${currency}</p>
+            <p><strong>Region:</strong> ${country.region}</p>
+            <p><strong>Subregion:</strong> ${country.subregion || "N/A"}</p>
+        `;
+
+        oldContent.appendChild(newDiv);
     }
+}
